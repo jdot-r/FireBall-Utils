3d8243b135189e7b203b79889970afda7130d1d9
CøsʍicSαѵαɢe <jross3740@gmail.com>
2016-07-24 16:26:36 -0400

    Update Main.php

--- src/Utils/Main.php
+++ src/Utils/Main.php
@@ -1,1 +1,22 @@
+<?php
 
+namespace Utils;
+
+use pocketmine\plugin\PluginBase as Base;
+
+//config later, maybe
+
+class Main extends Base{
+  
+  public function onLoad(){
+    $this->getLogger()->notice("This plugin is a complete experiment, thanks for testing!");
+  }
+  
+  public function onEnable(){
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);
+  }
+  
+  public function onDisable(){
+    return true;
+  }
+}
