05104c76596e97e9664cbc67d2a595f66af29cb9
Cøsʍic Sαѵαɢe <jross3740@gmail.com>
2016-09-01 16:20:07 -0400

    Update Listener.php

--- src/Utils/Listener.php
+++ src/Utils/Listener.php
@@ -13,42 +13,45 @@ use pocketmine\Player;
 
 use Utils\projectiles\FireBall;
 
-use pocketmine\level\sound\BlazeShootSound;
-
 use pocketmine\Server;
 
 use pocketmine\utils\Config;
 use pocketmine\utils\TextFormat as tf;
 
-class Listener extends PluginBase implements Listener{
+class Listener extends Listener{
   
   public function onDamage(EntityDamageEvent $action){
     $e = $action->getEntity();
+    $random = $this->config->get("percent.chance");
     if($action instanceof EntityDamageByEntityEvent){
       if(!$e instanceof Player && $e->hasPermission("fireball.use")){
           return true;
         }
-        $f = 1.0;
-        $nbt = new CompoundTag("", [
-          "Pos" => new ListTag("Pos", [
-          new DoubleTag("", $player->x),
-          new DoubleTag("", $player->y + $player->getEyeHeight()),
-          new DoubleTag("", $player->z)
-          ]),//list tag pos
-          "Motion" => new ListTag("Motion", [
-            new DoubleTag("", - \sin ( $player->yaw / 180 * M_PI ) *\cos ( $player->pitch / 180 * M_PI )),
-            new DoubleTag("", - \sin ( $player->pitch / 180 * M_PI )),
-        		new DoubleTag("", \cos ( $player->yaw / 180 * M_PI ) *\cos ( $player->pitch / 180 * M_PI ))
-        		]), //list tag motion
-        		"Rotation" => new ListTag("Rotation", [
-        		  new FloatTag("", lcg_value() * 360),
-        			new FloatTag("", 0)
-        			]), //list tag rotation
-        			]);     //WHOLE COMPOUND TAG
-        			$bomb = Entity::createEntity("FireBall", $player->getLevel()->getChunk($player->x >> 4, $player->z >> 4), $nbt);
-        			$bomb->setMotion($bomb->getMotion()->multiply($f));
-        			$bomb->setOnFire(PHP_INT_MAX);
-        			$bomb->spawnToAll();
+        switch(mt_rand(1, $random)){
+          case 1:
+            $f = 1.0;
+            $nbt = new CompoundTag("", [
+              "Pos" => new ListTag("Pos", [
+                new DoubleTag("", $player->x),
+                new DoubleTag("", $player->y + $player->getEyeHeight()),
+                new DoubleTag("", $player->z)
+                ]),
+                "Motion" => new ListTag("Motion", [
+                  new DoubleTag("", - \sin ( $player->yaw / 180 * M_PI ) *\cos ( $player->pitch / 180 * M_PI )),
+                  new DoubleTag("", - \sin ( $player->pitch / 180 * M_PI )),
+        		      new DoubleTag("", \cos ( $player->yaw / 180 * M_PI ) *\cos ( $player->pitch / 180 * M_PI ))
+        		      ]),
+        		      "Rotation" => new ListTag("Rotation", [
+        		        new FloatTag("", lcg_value() * 360),
+        			      new FloatTag("", 0)
+        			      ]),
+        			      ]);
+        			      $bomb = Entity::createEntity("FireBall", $player->getLevel()->getChunk($player->x >> 4, $player->z >> 4), $nbt);
+        			      $bomb->setMotion($bomb->getMotion()->multiply($f));
+        			      $bomb->setOnFire(true);
+        			      $bomb->spawnToAll();
+                  break;
+        }
     }
   }
-  }
+}
